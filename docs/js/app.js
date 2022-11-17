1fce897a40c690ff54eff467730947f814b0e018
Tvisher <tvisher@mail.ru>
2022-11-18 00:14:22 +0600

    add slider tempalate

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,5 @@
-
-async function testWebP() {
+// Проверка поддержки WebP
+(async function testWebP() {
     const hasWebP = await new Promise(res => {
         const webP = new Image();
         webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
@@ -9,8 +9,5 @@ async function testWebP() {
     });
     let className = hasWebP === true ? 'webp' : 'no-webp';
     document.documentElement.classList.add(className);
-};
-
-// Проверка поддержки webP
-testWebP();
+})();
 
